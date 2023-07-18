80d11df678221a658c0e41e20f5a9af060558fda
Sophart <phunsophart@gmail.com>
2023-07-18 17:52:54 +0700

    Add Movie Model

---
+++ SophartVidly/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using SophartVidly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SophartVidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movie = new Movie()
+            {
+                Name = "Die Hard!"
+            };
+
+            return View(movie);
+        }
+    }
+}
