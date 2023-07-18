8298532a94fde5ad7c2cb7f2bd30917b1412c58f
Sophart <phunsophart@gmail.com>
2023-07-18 17:39:58 +0700

    Add project files.

---
+++ SophartVidly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SophartVidly.MvcApplication" Language="C#" %>
---
+++ SophartVidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SophartVidly.Startup))]
+namespace SophartVidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
