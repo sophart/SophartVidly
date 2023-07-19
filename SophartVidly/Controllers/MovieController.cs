8c31367f9b1b1e4248e6d61eb74f3317d82b0634
Sophart <phunsophart@gmail.com>
2023-07-19 17:49:45 +0700

    Pass Data to View Using ViewModel

--- SophartVidly/Controllers/MovieController.cs
+++ SophartVidly/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using SophartVidly.Models;
+using SophartVidly.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,11 +17,20 @@ namespace SophartVidly.Controllers
                 Name = "Die Hard!"
             };
 
-            // ViewData["movies"] = movie;
+            var customers = new List<Customer>()
+            {
+             new Customer  { Id = Guid.NewGuid(), Name ="Sophart"},
+             new Customer  { Id = Guid.NewGuid(), Name = "Lakkhena"},
+             new Customer  { Id = Guid.NewGuid(), Name = "Panhakvoan"},
+            };
 
-            // ViewBag.Movie = movie;
+            var viewModel = new MovieViewModel
+            {
+                Customers = customers,
+                Movie = movie
+            };
 
-            return View();
+            return View(viewModel);
         }
 
     }
---
+++ SophartVidly/ViewModels/MovieViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SophartVidly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SophartVidly.ViewModels
+{
+    public class MovieViewModel
+    {
+        public Movie Movie { get; set; }
+
+        public List<Customer> Customers { get; set; }
+    }
+}
