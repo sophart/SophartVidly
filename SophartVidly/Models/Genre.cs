130bab67ba81af8f583efdc07956693b76e31a0a
Sophart <phunsophart@gmail.com>
2023-07-20 11:29:59 +0700

    Add Genre And Movie to Database

--- SophartVidly/Models/Genre.cs
+++ SophartVidly/Models/Genre.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace SophartVidly.Models
 {
-    public class Movie
+    public class Genre
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
 
-        public Genre Genre { get; set; }
+        public List<Movie> Movies { get; set; }
     }
 }
