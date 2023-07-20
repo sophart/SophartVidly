8af91001a6c34910ad2c7bb94a49a3d829fb1002
Sophart <phunsophart@gmail.com>
2023-07-20 11:41:08 +0700

    Add ReleaseDate to Movie

--- SophartVidly/Models/Movie.cs
+++ SophartVidly/Models/Movie.cs
@@ -8,7 +8,10 @@ namespace SophartVidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
 
         public Genre Genre { get; set; }
     }
